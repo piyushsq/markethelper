8c22f20ba2e5d4c0e2dee754a0cb5115c7f6dcdd
piyushsq <piyushsuteri12@gmail.com>
2025-07-14 20:51:52 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,30 @@
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open('market-cache').then(function (cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
-      ]);
-    })
+const CACHE_NAME = 'market-helper-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/service-worker.js',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
-  );
+// Fetch event
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    // Always serve index.html for navigation (like refresh)
+    event.respondWith(
+      caches.match('/index.html').then(response => response || fetch('/index.html'))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(response => response || fetch(event.request))
+    );
+  }
 });
